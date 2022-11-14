2228ad0cb4e53d09034711a0c18b42fe5862fbc7
Matteo Bertucci <matteobertucci2004@gmail.com>
2022-11-14 07:19:06 +0000

    Pending changes exported from your codespace

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-qemu-system-x86_64 -kernel ./vmlinuz -append "console=ttyS0 -- $@" -initrd ./initramfs.cpio.gz -nographic
+qemu-system-x86_64 -m 1G -kernel ./vmlinuz -append "console=ttyS0 -- $@" -initrd ./initramfs.cpio.gz -nographic
